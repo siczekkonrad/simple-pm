676da86e8d22dd76b74d1d59f101877c9ab1d9c0
Konrad Siczek <konradd.siczek@gmail.com>
2019-10-18 15:30:57 +0200

    Add correct task type change

--- src/components/TaskForm/TaskForm.component.js
+++ src/components/TaskForm/TaskForm.component.js
@@ -9,12 +9,20 @@ const TaskForm = (props) => {
     const resetInput = () => {
         setTask('')
     }
+    const resetListType = () => {
+        setType('')
+    }
     const addTask = (e) => {
         e.preventDefault();
         setTask(e.target.value);
 
     }
 
+    const submitForm = () => {
+        if(task !== '' && listType !== '') {
+            submitTask(task)
+        }
+    }
     const submitTask = (task) => {
         props.addTask({
             title: task,
@@ -24,27 +32,26 @@ const TaskForm = (props) => {
             isArchived: false,
         })
         resetInput()
+        resetListType()
     }
 
     return (
-        <div className='TaskForm'>
+        <form className='TaskForm'  onSubmit={(e) => {
+            e.preventDefault();
+            submitForm();
+           }}>
             <div className='TaskForm__text'>
                 <input className='TaskForm__input'
                    value={task}
                    onChange={addTask}/>
             </div>
             <div className='TaskForm__radio'>
-                <input type='radio' name='list' value='active' onChange={(e) => setType(e.target.value)}/>Active <br/>
-                <input type='radio' name='list' value='done' onChange={(e) => setType(e.target.value)}/>Done <br/>
-                <input type='radio' name='list' value='archived' onChange={(e) => setType(e.target.value)}/>Archived <br/>
+                <input type='radio' name='list' value='active' required='required' onChange={(e) => setType(e.target.value)}/>Active <br/>
+                <input type='radio' name='list' value='done' required='required' onChange={(e) => setType(e.target.value)}/>Done <br/>
+                <input type='radio' name='list' value='archived' required='required' onChange={(e) => setType(e.target.value)}/>Archived <br/>
             </div>
-            <button className='TaskForm__button'
-                onClick={() => {
-                    if(task !== '') {
-                        submitTask(task)
-                    }}}
-            >Add task</button>
-        </div>
+            <button className='TaskForm__button'>Add task</button>
+        </form>
     )
 }
 
